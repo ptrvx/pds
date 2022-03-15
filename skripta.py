ad3fdf431d51ac1dc3573b01403d7d78c527937f
Petar Vukovic <pvukovic@raf.edu.yu>
2022-03-15 18:00:27 +0100

    prvi komit

---
+++ skripta.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+url_posts = "http://jsonplaceholder.typicode.com/posts"
+url_users = "http://jsonplaceholder.typicode.com/users"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url_posts, headers=headers, data=payload)
+
+data = json.loads(response.text)
+
+body = data[10].get("body")
+title = data[10].get("title")
+
+payload = json.dumps({
+  "title": title,
+  "body": body,
+  "userId": 1
+})
+
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url_posts, headers=headers, data=payload)
+
+print(response.text)
+
+

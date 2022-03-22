51e9a39a71f3c70bca3d9d3006178a6cbc488034
Petar Vukovic <pvukovic@raf.edu.yu>
2022-03-22 17:29:54 +0100

    izmene na masteru

--- skripta2.py
+++ skripta2.py
@@ -3,14 +3,22 @@ import json
 
 conn = http.client.HTTPSConnection("jsonplaceholder.typicode.com")
 payload = json.dumps({
-  "title": "Najnoviji naslov",
-  "body": "Najnoviji tekst",
+  "title": "Jos noviji naslov",
+  "body": "Jos noviji tekst",
   "userId": 3
 })
 headers = {
   'Content-Type': 'application/json'
 }
+
+
+def testFunction():
+  print("testing...")
+
+
+
 conn.request("POST", "/posts", payload, headers)
 res = conn.getresponse()
 data = res.read()
 print(data)
+
